1c1473e1010c39b19ac5c620fe82a66e4642bb79
suraj shetty <surajshetty3416@gmail.com>
2016-03-26 10:39:17 +0530

    First Commit

---
+++ app/scripts/controllers/mainController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name Bling.controller:MainController
+ * @description
+ * # MainController
+ */
+angular.module('Bling')
+  .controller('MainController', function($scope) {
+
+    // do something with $scope
+
+  });
---
+++ app/scripts/controllers/settingsController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name Bling.controller:SettingsController
+ * @description
+ * # SettingsController
+ */
+angular.module('Bling')
+  .controller('SettingsController', function($scope) {
+
+    // do something with $scope
+
+  });
