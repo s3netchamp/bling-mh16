068db5de68ffb286a86bceda2246b8323164d59f
suraj shetty <surajshetty3416@gmail.com>
2016-03-27 13:08:17 +0530

    Poll functionality update

--- app/scripts/services/SendQuestion.js
+++ app/scripts/services/SendQuestion.js
@@ -7,60 +7,83 @@
  * # SendQuestion
  * SendQuestion ref
  */
- angular.module('Bling')
-  // use factory for services
-  .factory('SendQuestion', function(FirebaseRef, $q) {
+angular.module('Bling')
+    // use factory for services
+    .factory('SendQuestion', function(FirebaseRef, $q) {
 
-    function chatKey(phone1, phone2) {
-      return (phone1 > phone2) ? phone1+'_'+phone2 : phone2 +'_'+ phone1;
-    }
-
-    return {
-
-      single: function (from, to, q) {
-
-        var defer = $q.defer();
-
-        function setQ() {
-          return FirebaseRef.child('questions/'+chatKey(from, to)).push({
-            question: q,
-            sender: from
-          });
+        function chatKey(phone1, phone2) {
+            return (phone1 > phone2) ? phone1 + '_' + phone2 : phone2 + '_' + phone1;
         }
-        function updateUserQ() {
-          var obj = {};
-          obj[chatKey(from,to)] = true;
-          return FirebaseRef.child('users/'+from+'/chatKeys').update(obj);
-        }
-        setQ().then(updateUserQ).then(function (res) {
-          defer.resolve('success', res);
-        }, function(err){
-          defer.reject(err);
-        });
-        return defer.promise;
 
-      },
-      answer : function (chatKey, questionKey, opt) {
+        return {
+            
+            createPoll: function (from,q) {
+                var defer = $q.defer();
+                function setPoll() {
+                    return FirebaseRef.child('polls/'+q.pollId ).set({
+                        pollSubject: q.pollSubject,
+                        options : q.options,
+                        sender: from
+                    });
+                }
+                
+                function updateUserPoll() {
+                    var obj = {};
+                    obj[q.pollId] = true;
+                    return FirebaseRef.child('users/' + from + '/pollIds').update(obj);
+                }
+                setPoll().then(updateUserPoll).then(function(res) {
+                    defer.resolve('success', res);
+                }, function(err) {
+                    defer.reject(err);
+                });
+                return defer.promise;
+            },
 
-        var defer = $q.defer();
+            single: function(from, to, q) {
 
-        FirebaseRef.child('questions/'+chatKey+'/'+questionKey).update({
-          answered: true,
-          answer: {
-            id: opt.id,
-            text: opt.text
-          }
-        }, function (err) {
-          if(!err){
-            defer.resolve(questionKey);
-          }
-          else{
-            defer.reject(err);
-          }
-        });
-        return defer.promise;
-      }
+                var defer = $q.defer();
 
-    };
+                function setQ() {
+                    return FirebaseRef.child('questions/' + chatKey(from, to)).push({
+                        question: q,
+                        sender: from
+                    });
+                }
+                function updateUserQ() {
+                    var obj = {};
+                    obj[chatKey(from, to)] = true;
+                    return FirebaseRef.child('users/' + from + '/chatKeys').update(obj);
+                }
+                setQ().then(updateUserQ).then(function(res) {
+                    defer.resolve('success', res);
+                }, function(err) {
+                    defer.reject(err);
+                });
+                return defer.promise;
 
-  });
+            },
+            answer: function(chatKey, questionKey, opt) {
+
+                var defer = $q.defer();
+
+                FirebaseRef.child('questions/' + chatKey + '/' + questionKey).update({
+                    answered: true,
+                    answer: {
+                        id: opt.id,
+                        text: opt.text
+                    }
+                }, function(err) {
+                    if (!err) {
+                        defer.resolve(questionKey);
+                    }
+                    else {
+                        defer.reject(err);
+                    }
+                });
+                return defer.promise;
+            }
+
+        };
+
+    });
